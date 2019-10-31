3dda8c2ab3f6797bc8236219128c74211a79836e
David María Arribas <dmariaa70@gmail.com>
2019-10-31 18:49:39 +0100

    Initial version. it learnsgit status

---
+++ Assets/Scripts/Practica1/QMind.cs
@@ -0,0 +1,115 @@
+﻿using Assets.Scripts;
+using Assets.Scripts.DataStructures;
+using UnityEngine;
+
+namespace Practica1
+{
+    public class QMind : AbstractPathMind
+    {
+        [SerializeField]
+        public bool UsePreviousLearning = false;
+
+        [SerializeField]
+        public float alpha = 0.3f;
+
+        [SerializeField]
+        public float gamma = 0.8f;
+
+        private QTable qtable;
+
+        private float GetReward(CellInfo cell)
+        {
+            // Ha intentado moverse a una celda no transitable
+            if(cell==null || !cell.Walkable)
+            {
+                return -1.0f;
+            }
+
+            if(cell.ItemInCell != null && cell.ItemInCell.Type==PlaceableItem.ItemType.Goal)
+            {
+                return 100.0f;
+            }
+
+            return 0.0f;
+        }
+
+
+        private CellInfo getAleatNavigableCell(CellInfo currentPos, BoardInfo boardInfo, out Locomotion.MoveDirection direction)
+        {
+            CellInfo[] walkableNeighbours = currentPos.WalkableNeighbours(boardInfo);
+            CellInfo newPos;
+
+            do
+            {
+                int randomValue = Random.Range(0, walkableNeighbours.Length);
+                newPos = walkableNeighbours[randomValue];
+                direction = (Locomotion.MoveDirection)randomValue;
+            } while (newPos == null);
+
+            return newPos;
+        }
+
+        public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
+        {
+            if(qtable==null)
+            {
+                Learn(boardInfo);
+            }
+
+            // Proxima celda aleatoria
+            Locomotion.MoveDirection nextDirection = (Locomotion.MoveDirection) qtable.getNextSavedDirection(currentPos);
+            
+            /*
+            CellInfo nextCell = getAleatNavigableCell(currentPos, boardInfo, out nextDirection);
+
+            // Recalculo de Q
+            float r = GetReward(nextCell);
+            float Q = qtable[currentPos, nextDirection];
+            float nextQMax = qtable.getHighestQAction(nextCell);
+            qtable[currentPos, nextDirection] = (1 - alpha) * Q + alpha * (r + gamma * nextQMax);
+            */
+            
+            return nextDirection;
+        }
+
+        public override void Repath()
+        {
+        }
+
+        private void Learn(BoardInfo boardInfo)
+        {
+            CellInfo nextState, initialState;
+            qtable = new QTable(boardInfo);
+
+            for (int episode = 0; episode < 10000; episode++)
+            {
+                initialState = boardInfo.CellInfos[
+                    Random.Range(0, boardInfo.NumColumns),
+                    Random.Range(0, boardInfo.NumRows)
+                ];
+
+                bool endOfEpisode = false;
+
+                do
+                {
+                    int randomDirection = Random.Range(0, 4);
+                    nextState = initialState.WalkableNeighbours(boardInfo)[randomDirection];
+                    float r = GetReward(nextState);
+                    float Q = qtable[initialState, (Locomotion.MoveDirection)randomDirection];
+
+                    float nextQMax = nextState != null ? qtable.getHighestQAction(nextState) : 0;
+                    qtable[initialState, (Locomotion.MoveDirection)randomDirection] = (1 - alpha) * Q + alpha * (r + gamma * nextQMax);
+
+                    if (r == -1 || r == 100)
+                    {
+                        endOfEpisode = true;
+                    }
+
+                    initialState = nextState;
+                } while (!endOfEpisode);
+                
+                Debug.LogFormat("Episode {0} finished", episode);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Practica1/QTable.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts;
+using Assets.Scripts.DataStructures;
+using UnityEngine;
+
+namespace Practica1
+{
+    [Serializable]
+    public class QTable
+    {
+        [SerializeField]
+        private List<float> table;
+
+        private int numStates;
+        private int numActions;
+        private int boardColumns;
+        
+
+        public float this[CellInfo state, Locomotion.MoveDirection action]
+        {
+            get
+            {            
+                return table[getArrayPosition(state, action)];
+            }
+
+            set
+            {
+                table[getArrayPosition(state, action)] = value;
+            }
+        }
+
+        public List<float> getStateValues(CellInfo state)
+        {
+            return new List<float>
+            {
+                table[getArrayPosition(state, Locomotion.MoveDirection.Up)],
+                table[getArrayPosition(state, Locomotion.MoveDirection.Down)],
+                table[getArrayPosition(state, Locomotion.MoveDirection.Left)],
+                table[getArrayPosition(state, Locomotion.MoveDirection.Right)]
+            };
+        }
+
+        public int getNextSavedDirection(CellInfo state)
+        {
+            List<float> stateValues = getStateValues(state);
+            int nextDirection = 0;
+            float highestValue = 0;
+            
+            for(int i=0; i < stateValues.Count; i++)
+            {
+                if(stateValues[i] > highestValue)
+                {
+                    highestValue = stateValues[i];
+                    nextDirection = i;
+                }
+            }
+            return nextDirection;
+        }
+
+        public float getHighestQAction(CellInfo state)
+        {
+            float highestValue = 0;
+            List<float> stateValues = getStateValues(state);
+
+            for(int i=0; i < stateValues.Count; i++)
+            {
+                if(stateValues[i] > highestValue)
+                {
+                    highestValue = stateValues[i];
+                }
+            }
+
+            return highestValue;
+        }
+
+        public QTable()
+        {
+            int size = this.numStates * this.numActions;
+            table = new List<float>(size);
+        }
+
+        public QTable(BoardInfo board)
+        {
+            this.boardColumns = board.NumColumns;
+            this.numStates = board.NumColumns * board.NumRows;
+            this.numActions = System.Enum.GetValues(typeof(Locomotion.MoveDirection)).Length;
+            
+            int size = this.numStates * this.numActions;
+            table = new List<float>(size);
+            for(int i=0; i < size; i++)
+            {
+                table.Add(0);
+            }
+        }
+
+        private int getArrayPosition(CellInfo state, Locomotion.MoveDirection action)
+        {
+            int row = state.RowId * this.boardColumns + state.ColumnId;
+            int column = (int)action;
+            return row * numActions + column;
+        }
+    }
+}
